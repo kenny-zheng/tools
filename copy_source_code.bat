e045d3ceab66aa7e0532808c20911de738cf6ceb
kenny-zheng <zokmi.zheng@gmail.com>
2025-01-13 22:29:03 +0800

    Update copy_source_code.bat to allow folder selection and include subfolders

--- copy_source_code.bat
+++ copy_source_code.bat
@@ -2,23 +2,25 @@
 
 :: Get all directories in the current folder
 echo Current directories:
+setlocal enabledelayedexpansion
+set count=0
+
 for /d %%D in (*) do (
-    echo %%D
+    set /a count+=1
+    echo !count! - %%D
+    set "folder[!count!]=%%D"
 )
 
-:: Prompt user to input folder name, default is DefaultFolder
-set /p FOLDER_NAME="Please enter the folder name to copy (default: DefaultFolder): "
-if "%FOLDER_NAME%"=="" (
-    set FOLDER_NAME=DefaultFolder
-)
+:: Prompt user to input folder number
+set /p FOLDER_INDEX="Please enter the folder number to copy: "
 
-:: Check if a folder name was provided
-if "%FOLDER_NAME%"=="" (
-    echo Please provide a folder name.
+:: Check if a valid number was provided
+if "!folder[%FOLDER_INDEX%]!"=="" (
+    echo Please provide a valid folder number.
     exit /b 1
 )
 
-set PROJECT_PATH=%FOLDER_NAME%
+set PROJECT_PATH=!folder[%FOLDER_INDEX%]!
 set DESTINATION=SourceCode
 
 :: Create SourceCode folder if it doesn't exist
@@ -26,8 +28,9 @@ if not exist "%DESTINATION%" (
     mkdir "%DESTINATION%"
 )
 
-:: Copy source code to SourceCode folder, excluding bin and obj folders
+:: Copy source code to SourceCode folder, including all subfolders and excluding bin and obj folders
 xcopy "%PROJECT_PATH%\*" "%DESTINATION%\" /E /I /EXCLUDE:bin\*;obj\* /Y
 
 echo Source code has been successfully copied to %DESTINATION% folder.
 pause
+endlocal
