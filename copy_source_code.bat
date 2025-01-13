9d562d8cfbac1837b52ea18f8c48432b13c4c30f
Zhong-Min Zheng <zokmi.zheng@gmail.com>
2025-01-14 00:10:26 +0800

    Add files via upload

--- copy_source_code.bat
+++ copy_source_code.bat
@@ -1,10 +1,10 @@
 @echo off
-
-:: Get all directories in the current folder
-echo Current directories:
+:: Enable delayed expansion for variables inside loops
 setlocal enabledelayedexpansion
-set count=0
 
+:: Get all directories in the current folder and display them with index numbers
+echo Current directories:
+set count=0
 for /d %%D in (*) do (
     set /a count+=1
     echo !count! - %%D
@@ -14,39 +14,36 @@ for /d %%D in (*) do (
 :: Prompt user to input folder number
 set /p FOLDER_INDEX="Please enter the folder number to copy: "
 
-:: Check if a valid number was provided
+:: Validate the user input
 if "!folder[%FOLDER_INDEX%]!"=="" (
     echo Please provide a valid folder number.
+    pause
     exit /b 1
 )
 
-set PROJECT_PATH=!folder[%FOLDER_INDEX%]!
-set DESTINATION=SourceCode
+set "PROJECT_PATH=!folder[%FOLDER_INDEX%]!"
+set "DESTINATION=SourceCode"
 
-:: Create SourceCode folder if it doesn't exist
+:: Create the destination folder if it doesn't exist
 if not exist "%DESTINATION%" (
     mkdir "%DESTINATION%"
 )
 
-:: Copy source code to SourceCode folder, excluding bin and obj folders recursively
-for /d %%D in ("%PROJECT_PATH%\*") do (
-    if /i not "%%~nxD"=="bin" (
-        if /i not "%%~nxD"=="obj" (
-            xcopy "%%D\*" "%DESTINATION%\" /E /I /Y
-        )
-    )
+:: Use robocopy to copy the source code while excluding:
+:: - bin folders
+:: - obj folders
+:: - directories starting with a dot (e.g. .git, .svn)
+:: /E copies subdirectories (including empty ones)
+:: /XD excludes directories matching the provided names or patterns
+robocopy "%PROJECT_PATH%" "%DESTINATION%" /E /XD bin obj ".*"
+
+:: Check the robocopy exit code for success or partial success
+:: 0 and 1 are considered successful copies
+if errorlevel 2 (
+    echo An error occurred during copying.
+) else (
+    echo Source code has been successfully copied to the "%DESTINATION%" folder.
 )
 
-:: Check for nested directories and exclude bin and obj folders
-for /d %%D in ("%DESTINATION%\*") do (
-    if /i "%%~nxD"=="bin" (
-        rmdir /S /Q "%%D"
-    )
-    if /i "%%~nxD"=="obj" (
-        rmdir /S /Q "%%D"
-    )
-)
-
-echo Source code has been successfully copied to %DESTINATION% folder.
 pause
 endlocal
